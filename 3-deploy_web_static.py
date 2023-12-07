805f401778450fc9df2c4f1936c13c122af6bb57
kucheli mutambi <kuchelideogratius@gmail.com>
2023-12-07 16:46:49 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,7 +10,7 @@ Usage:
 from fabric.api import local, env, put, run
 from time import strftime
 import os.path
-env.hosts = ['35.229.54.225', '35.231.225.251']
+env.hosts = ['100.25.161.237', '54.197.21.169']
 
 
 def do_pack():
